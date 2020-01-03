cd1ede86278790f0c8ce7ed22f311fe646557a59
Ricardo Reis <ricardo.areis@gmail.com>
2020-01-03 13:48:24 -0300

    added a little sh to clone some plugins

---
+++ addPlugins.sh
@@ -0,0 +1,4 @@
+ git clone https://github.com/tmux-plugins/tpm                      ~/.tmux/plugins/tpm
+ git clone https://github.com/romkatv/powerlevel10k                 ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+ git clone https://github.com/zsh-users/zsh-autosuggestions         ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
