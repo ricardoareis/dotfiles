bb7e32b1b0a56835c77ba9f8a9ea344bca7b541d
Ricardo Reis <ricardo.areis@gmail.com>
2020-01-19 18:08:26 -0300

    removing git clone

--- addPlugins.sh
+++ addPlugins.sh
@@ -1,5 +1,1 @@
- git clone https://github.com/tmux-plugins/tpm                      ~/.tmux/plugins/tpm
- git clone https://github.com/romkatv/powerlevel10k                 ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
- git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
- git clone https://github.com/zsh-users/zsh-autosuggestions         ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
- brew install fd fzf gawk
+ brew install fd fzf gawk bash
