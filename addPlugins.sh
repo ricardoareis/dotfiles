07053e3fc8480d3e2e8b2891ad6e056d5aa7b5dd
Ricardo Reis <ricardo.areis@gmail.com>
2020-01-19 17:48:49 -0300

    little refactoring

--- addPlugins.sh
+++ addPlugins.sh
@@ -2,4 +2,4 @@
  git clone https://github.com/romkatv/powerlevel10k                 ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
  git clone https://github.com/zsh-users/zsh-autosuggestions         ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
- brew install fd fzf
+ brew install fd fzf gawk
