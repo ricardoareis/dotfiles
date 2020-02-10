dbcdd7c19e1f76d99778651b60c5b57e9d5573c1
Ricardo Reis <ricardo.areis@gmail.com>
2020-02-10 16:37:37 -0300

    zinit w/ delayed load config

--- zsh_custom/zinit.zsh
+++ zsh_custom/zinit.zsh
@@ -1,3 +1,9 @@
 # Two regular plugins loaded without tracking.
-zinit light zsh-users/zsh-autosuggestions
+zinit ice silent wait"2" blockf atpull'zinit creinstall -q .'
+zinit light zsh-users/zsh-completions
+
+zinit ice silent wait"2" atinit"zpcompinit; zpcdreplay"
 zinit light zdharma/fast-syntax-highlighting
+
+zinit ice silent wait"2" atload"_zsh_autosuggest_start"
+zinit light zsh-users/zsh-autosuggestions
