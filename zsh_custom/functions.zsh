d7321aa28341d7a985f5ab5b916cac6ce2a97a51
Ricardo Reis <ricardo.areis@gmail.com>
2020-03-06 11:42:02 -0300

    refactoring to solve path structure

--- zsh_custom/functions.zsh
+++ zsh_custom/functions.zsh
@@ -135,3 +135,30 @@ function .4() {cd ../../../../}
 function .5() {cd ../../../../../}
 function .6() {cd ../../../../../../}
 function .7() {cd ../../../../../../../}
+
+# rationalize-path()
+# Later we'll need to trim down the paths that follow because the ones
+# given here are for all my accounts, some of which have unusual
+# paths in them.  rationalize-path will remove
+# nonexistent directories from an array.
+rationalize-path () {
+  #http://zsh.sourceforge.net/Contrib/startup/users/debbiep/dot.zshenv
+  # Note that this works only on arrays, not colon-delimited strings.
+  # Not that this is a problem now that there is typeset -T.
+  local element
+  local build
+  build=()
+  # Evil quoting to survive an eval and to make sure that
+  # this works even with variables containing IFS characters, if I'm
+  # crazy enough to setopt shwordsplit.
+  eval '
+  foreach element in "$'"$1"'[@]"
+  do
+    if [[ -d "$element" ]]
+    then
+      build=("$build[@]" "$element")
+    fi
+  done
+  '"$1"'=( "$build[@]" )
+  '
+}
