c2d5e3f4dc1d323b9be4012ccd11ffdda8d1a07d
Ricardo Reis <ricardo.areis@gmail.com>
2020-03-17 21:36:07 -0300

    adjusted for autoload

--- zsh_custom/functions/getRecentDir.zsh
+++
@@ -1,8 +0,0 @@
-# Function: recent directory with fasd ##{
-# vim: set expandtab sw=4 ts=4 sts=4 et tw=78 ft=zsh foldmarker=##{,##} foldlevel=0 foldmethod=marker spell:
-function rdf() {
-  local dir
-  dir=$(fasd -Rdl | fzf --no-sort +m) && cd "$dir"
-}
-zle -N rdf
-##}
