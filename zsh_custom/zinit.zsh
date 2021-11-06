c4a8ff3c583ffebc1bbc0befd66ea328b72db468
Ricardo Reis <ricardo.areis@gmail.com>
2021-11-06 15:45:25 -0300

    removed osx plugin

--- zsh_custom/zinit.zsh
+++ zsh_custom/zinit.zsh
@@ -38,9 +38,6 @@ zinit ice svn wait"1a" lucid
 zinit snippet OMZ::plugins/aws
 
 zinit ice svn wait"1a" lucid
-zinit snippet OMZ::plugins/osx
-
-zinit ice svn wait"1a" lucid
 zinit snippet OMZ::plugins/colored-man-pages
 
 zinit ice svn wait"1a" lucid
