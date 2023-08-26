b13e28139e8ec9bbcbccaaa1cbaa25c5671400e3
Ricardo Reis <ricardo.areis@gmail.com>
2023-08-26 19:05:52 -0300

    solved the necessity of these alias

--- zsh_custom/alias.zsh
+++ zsh_custom/alias.zsh
@@ -7,5 +7,5 @@ alias tn="t new -t"
 alias tq="tmux kill-pane"
 [[ "$(uname)" == "Darwin"  ]] && \
     alias updatedb="sudo /usr/libexec/locate.updatedb" && \
-    alias vim="~/repos/vim/src/vim"
-    alias vimdiff="~/repos/vim/src/vim -d"
+    #alias vim="~/repos/vim/src/vim"
+    #alias vimdiff="~/repos/vim/src/vim -d"
